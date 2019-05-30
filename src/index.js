fca33a33a580bb669acf0319fcdb383986dd8383
GucodeDev <49602560+GucodeDev@users.noreply.github.com>
2019-05-30 13:54:39 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> </h1>
+      
+    </div>
+  );
+}
+console.log(document.querySelector('div').innerHTML="alterado");
